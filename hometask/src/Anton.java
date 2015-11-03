c5ddbb19468d9f1c07969c7ecff2cf1872ae8388
gravie1997 <gdemoney@mail.ru>
2015-11-03 21:56:07 +0200

    origin

---
+++ hometask/src/Anton.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Admin on 10/24/2015.
+ */
+public class Anton {
+    public static void main(String[] args){
+        int a=5;
+        int b=3;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+
+    }
+    }
